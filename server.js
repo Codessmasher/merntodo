7bac7092a9d1e9ec200f66a88a8f74c3f0b14217
DEEPJYOTI DAS <95518873+Codessmasher@users.noreply.github.com>
2024-02-06 21:21:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,24 @@ import router from "./routes/router.js";
 import mongoose from "mongoose"; 
 import dotenv from "dotenv"; // Import dotenv
 
-dotenv.config(); // Load environment variables from .env file
-
-const app = express();
-
+dotenv.config();  
+const app = express();  
 // Middlewares
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
-app.use("/api/users", router);
+app.use("/api/users", router); 
 
-// Use the .ENV environment variableS
-const PORT = process.env.PORT || 5000; 
-const MONGODB_URI = process.env.MONGODB_URI;  
+// Use the .ENV environment variables
+const PORT = process.env.PORT;  
+const MONGODB_URI = process.env.MONGODB_URI;   
 const DB_NAME = process.env.DB_NAME;  
+const API_URL = process.env.API_URL;  
 
 //mongodb+express connection
-mongoose.connect(   
+mongoose.connect(    
     `${MONGODB_URI}/${DB_NAME}`    
 )
 .then(() => app.listen(PORT, () => {
-    console.log(`Server is running on  localhost:${PORT}`); 
+    console.log(`Server is running on  ${API_URL}`); 
 }))
 .catch((err) => console.log(err))
