6967c6d53f7aec146e9d3cda69c5902a6c86b223
DEEPJYOTI DAS <95518873+Codessmasher@users.noreply.github.com>
2024-02-06 21:18:12 +0530

    Update verifyAuth.js

--- controller/verifyAuth.js
+++ controller/verifyAuth.js
@@ -1,9 +1,12 @@
 import jwt from "jsonwebtoken"; // Import jsonwebtoken  
+import dotenv from "dotenv"; // Import dotenv
+dotenv.config();
+const secret = process.env.SECRET; 
 
 export const verifyAuth = (req, res, next) => {
     const token = req.header('Authorization').replace('Bearer ', ''); 
     try {
-      const decoded = jwt.verify(token, 'THISISGOINGTOBEATODOWEBAPP'); // replace with your actual secret key
+      const decoded = jwt.verify(token, secret);  
       req.user = decoded;
       next();
     } catch (error) {
